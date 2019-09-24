e5277e67d5462082b656729932b07c3bfe9fd455
Wilson Manzano <wilson.manzano@qrvey.com>
2019-09-24 10:46:29 -0500

    Fixed showed firebase credentials

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBfX6trYD_1UCHK-5B_T8dL9a-Lg-aAC9Q",
-    authDomain: "e-commerce-b0881.firebaseapp.com",
-    databaseURL: "https://e-commerce-b0881.firebaseio.com",
-    projectId: "e-commerce-b0881",
-    storageBucket: "",
-    messagingSenderId: "336594049178",
-    appId: "1:336594049178:web:a4017c87681c828562e723"
-};
+import { firebaseConfig } from './firebase.config';
 
 firebase.initializeApp(firebaseConfig);
 
