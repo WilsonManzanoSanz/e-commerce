9fd9d33a0f0cc065d2af8f9418775b8a36a31778
Wilson Manzano <wilson.manzano@qrvey.com>
2019-10-07 16:05:33 -0500

    Add number to cart icon on the navbar

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
 
 import './cart-dropdown.style.scss';
 
-const Cart = () => (
+const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            <Button>GO TO CHECKOUT</Button>
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+            } 
         </div>
+        <Button>GO TO CHECKOUT</Button>
     </div>
-)
+);
 
-export default Cart;
+const mapStateProps = ({ cart: { cartItems } }) => ({
+    cartItems
+})
+
+export default connect(mapStateProps)(CartDropdown);
