14dc5d1a28da3b55604273fea7a80dc4420ba8d9
Wilson Manzano <wilson.manzano@qrvey.com>
2019-10-07 17:08:00 -0500

    Added selector for the users and hidden dropdown

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import NotFound from './pages/notfound/notfound.component';
 import Navbar from './components/navbar/navbar.component.jsx';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.action';
+import { createStructuredSelector } from 'reselect';
+import { selectCurrentUser} from './redux/user/user.selector';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -53,8 +55,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
