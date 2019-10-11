41f1f78122bcfd3d94ea9f172c24bac97e135c7f
Wilson Manzano <wilson.manzano@qrvey.com>
2019-10-11 17:33:45 -0500

    Added checkout add, reduce and remove elements with reducers

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from './cart.action';
-import { addItemToCart } from './cart.utils';
+import { TOGGLE_CART_HIDDEN, ADD_ITEM, CLEAR_ITEM_FORM_CART, REMOVE_ITEM } from './cart.action';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden : true,
@@ -16,6 +16,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state, 
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case CLEAR_ITEM_FORM_CART:
+            return {
+                ...state, 
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }
+        case REMOVE_ITEM:
+            return {
+                ...state, 
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             }
         default:
             return state;
